7bb59238432383f90561d08ef7f24dad80a80c1d
Miguel Tapia <miguel.angel.tapia@gmail.com>
2019-06-17 19:29:44 -0500

    nuevo proyecto funcionando

--- src/app/pages/nuevo-proyecto/nuevo-proyecto.component.ts
+++ src/app/pages/nuevo-proyecto/nuevo-proyecto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/services/shared.service';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-nuevo-proyecto',
@@ -6,22 +9,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nuevo-proyecto.component.css']
 })
 export class NuevoProyectoComponent implements OnInit {
-  mensajeErrorImg = '';
-  claseCargaImg = '';
-  porcentajeCargaImg = '';
-  submitted = false;
-  constructor() { }
+   submitted = false;
+   private itemsCollection: AngularFirestoreCollection<Cliente>;
+  items: Observable<Cliente[]>;
 
-  ngOnInit() {
+  item: Proyecto = {
+    nombre: '',
+    nombreBusqueda: '',
+    numero: '',
+    cliente: '',
+    key: '',
+    pais: '',
+    usuarioAlta: '',
+    fechaAlta: 0,
+    estado: 0,
+
+    }
+
+    constructor(private sharedService: SharedService, private afs: AngularFirestore) { }
+
+    ngOnInit() {
+      this.getInfo();
+    }
+
+    getInfo() {
+      this.itemsCollection = this.afs.collection<Cliente>('clients');
+      console.log(this.itemsCollection);
+      this.items = this.itemsCollection.valueChanges();
+    }
+
+    nuevoItem(){
+      this.item.pais = 'MX';
+      //this.item.usuarioAlta = keyUser;
+      this.item.fechaAlta = new Date().getTime();
+      this.item.nombreBusqueda = this.sharedService.corregirCaracteres(this.item.nombre);
+
+      const itemCollection = this.afs.collection<Proyecto>('projects');
+      itemCollection.add(this.item);
+      this.submitted = true;
+    }
+
+
+    cancel() {
+      this.sharedService.cancelar();
+    }
+
+
+
+
+
   }
-  getFile(event){
-
-  }
-
-  nuevoEmpleado(){
-
-  }
-  cancel(){
-
-  }
-}
