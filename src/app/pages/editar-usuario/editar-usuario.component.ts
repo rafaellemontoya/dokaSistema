b1f51a428d2416d978bd326896ecea03d6b4a5d1
Oscar <oscardmontoyag@gmail.com>
2019-10-06 18:27:51 -0500

    Editado editar-usuario.component.ts

--- src/app/pages/editar-usuario/editar-usuario.component.ts
+++ src/app/pages/editar-usuario/editar-usuario.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestoreDocument, AngularFirestore } from '@angular/fire/firestore';
+import { SharedService } from 'src/app/services/shared.service';
+import { ActivatedRoute } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-editar-usuario',
@@ -11,9 +16,93 @@ export class EditarUsuarioComponent implements OnInit {
   claseCargaImg = '';
   porcentajeCargaImg = '';
   submitted = false;
-  constructor() { }
+  mensajeErrorImg = '';
+  claseCargaImg = '';
+  porcentajeCargaImg: any ;
+  submitted = false;
+  imgError = false;
+  selectedFile = null;
+  estadoCargaImg = false;
+  uploadPercent: Observable<number>;
+  downloadURL: Observable<string>;
+
+
+  idRecibido = '';
+  private itemDoc: AngularFirestoreDocument<Usuario>;
+  itemRecibido: Observable<Usuario>;
+  item: Usuario = {
+    seccion: '',
+    modo: '',
+    nombre: '',
+    puesto: '',
+    usuario: '',
+    password: '',
+    permisos: {usuarios:false,
+                clientes:false,
+                proyectos: false,
+                materiales: false,
+                damage: false,
+                Usuario: false,
+                reportesObraWeb: false,
+                reporteEquipoDamageWeb:false,
+                reporteEnvioWeb:false,
+                reporteDevolucionesWeb:false,
+                reportesObraApp: false,
+                reporteEquipoDamageApp:false,
+                reporteEnvioApp:false,
+                reporteDevolucionesApp:false,
+                },
+    email: '',
+    passwordEmail: '',
+    nombreBusqueda: '',
+    key: '',
+    foto: '',
+    pais:'',
+    usuarioAlta:'',
+    fechaAlta:0,
+    estado:0,
+
+    };
+
+  constructor(private sharedService: SharedService,
+              private route: ActivatedRoute, private afs: AngularFirestore, private storage: AngularFireStorage) { }
 
   ngOnInit() {
+    window.scrollTo(0, 0);
+    this.route.params
+    .subscribe( parametros => {
+    this.idRecibido = parametros.id;
+    console.log(parametros.id);
+    this.obtenerInformacion(this.idRecibido);
+   });
+ }
+
+  obtenerInformacion(idRecibido) {
+
+    this.itemDoc = this.afs.doc<Usuario>('equipmentType/' + idRecibido);
+    this.itemDoc.valueChanges().subscribe(data => {
+      console.log(data);
+      this.item = data;
+    });
+
+
+    // console.log(this.item.nombre);
+
+  }
+
+
+  crearItem() {
+    this.item.pais = 'MX';
+
+    this.item.fechaAlta = new Date().getTime();
+    this.item.nombreBusqueda = this.sharedService.corregirCaracteres(this.item.nombre);
+    this.itemDoc.update(this.item);
+    this.submitted = true;
+
+    window.scrollTo(0, 0);
+  }
+  cancel() {
+    this.sharedService.cancelar();
   }
 
   getFile(event){
@@ -23,8 +112,6 @@ export class EditarUsuarioComponent implements OnInit {
   nuevoEmpleado(){
 
   }
-  cancel(){
-
-  }
+ 
 
 }
